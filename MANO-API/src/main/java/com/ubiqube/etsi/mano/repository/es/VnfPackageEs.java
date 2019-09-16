c273d3465b07c6cf7622d6bbb78c6d364b43265f
Vignaud Olivier <ovi@ubiqube.com>
2019-09-16 16:55:44 +0200

    Make ES compilable.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/es/VnfPackageEs.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/es/VnfPackageEs.java
@@ -1,6 +1,7 @@
 package com.ubiqube.etsi.mano.repository.es;
 
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.validation.constraints.NotNull;
@@ -13,7 +14,7 @@ public class VnfPackageEs implements VnfPackageRepository {
 	@Override
 	public VnfPkgInfo get(final String id) {
 		// TODO Auto-generated method stub
-		return null;
+		return new VnfPkgInfo();
 	}
 
 	@Override
@@ -25,7 +26,7 @@ public class VnfPackageEs implements VnfPackageRepository {
 	@Override
 	public VnfPkgInfo save(final VnfPkgInfo entity) {
 		// TODO Auto-generated method stub
-		return null;
+		return new VnfPkgInfo();
 
 	}
 
@@ -38,7 +39,7 @@ public class VnfPackageEs implements VnfPackageRepository {
 	@Override
 	public List<VnfPkgInfo> query(final String filter) {
 		// TODO Auto-generated method stub
-		return null;
+		return new ArrayList<>();
 	}
 
 	@Override
