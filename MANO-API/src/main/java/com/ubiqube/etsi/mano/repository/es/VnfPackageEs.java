648dcb683365e3d19a0b0e4248f02f058483de74
Olivier Vignaud <ovi@ubiqube.com>
2019-10-15 08:22:52 +0200

    Make things compilable.

--- MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/es/VnfPackageEs.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/es/VnfPackageEs.java
@@ -6,6 +6,9 @@ import java.util.List;
 
 import javax.validation.constraints.NotNull;
 
+import com.ubiqube.etsi.mano.model.nslcm.LcmOperationStateType;
+import com.ubiqube.etsi.mano.model.nslcm.sol003.LcmOperationType;
+import com.ubiqube.etsi.mano.model.nslcm.sol003.VnfLcmOpOcc;
 import com.ubiqube.etsi.mano.model.vnf.sol005.VnfPkgInfo;
 import com.ubiqube.etsi.mano.repository.VnfPackageRepository;
 
@@ -76,6 +79,24 @@ public class VnfPackageEs implements VnfPackageRepository {
 	public <T, U extends Class> T loadObject(@NotNull final String _id, final U t, final String _filename) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public VnfLcmOpOcc createLcmOpOccs(final String vnfInstanceId, final LcmOperationType terminate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateState(final VnfLcmOpOcc lcmOpOccs, final LcmOperationStateType processing) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void attachProcessIdToLcmOpOccs(@NotNull final String id, final String processId) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
--- MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/phys/VnfPackagePhys.java
+++ MANO-API/src/main/java/com/ubiqube/etsi/mano/repository/phys/VnfPackagePhys.java
@@ -42,3 +42,4 @@ public class VnfPackagePhys extends GenericBinaryRepository<VnfPkgInfo> implemen
 
 }
 
+
